5ffdc44decc0ee921cba99a086a42cf30cd6b374
tamermoghraby <tamermg8@gmail.com>
2023-02-11 17:17:03 +0200

    Api Gateway

---
+++ src/main/java/com/FallBackMethodController.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class FallBackMethodController {
+    
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User Service is taking longer than Expected." +
+        " Please try again later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return "Department Service is taking longer than Expected." +
+        " Please try again later";
+    }
+
+}
